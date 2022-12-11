16eae2bda0a3a17f839023e57781eb95e92ff21f
Sergio <boarderbare@gmail.com>
2022-12-11 13:04:23 +0400

    change run tests by group

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'q5skpa',
+  projectId: 'dkk4kq',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
